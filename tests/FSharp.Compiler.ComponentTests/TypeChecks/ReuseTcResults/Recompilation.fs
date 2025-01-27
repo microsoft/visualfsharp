292ed8faf73a0295ed0d821ab61b82c38bc7092a
Petr <psfinaki@users.noreply.github.com>
2025-01-27 14:43:41 +0100

    update

---
+++ tests/FSharp.Compiler.ComponentTests/TypeChecks/ReuseTcResults/Recompilation.fs
@@ -0,0 +1,43 @@
+﻿namespace TypeChecks.ReuseTcResultsTests
+
+open System.IO
+
+open FSharp.Test
+open FSharp.Test.Compiler
+
+open Xunit
+
+open TestFramework
+
+[<Collection(nameof NotThreadSafeResourceCollection)>]
+type Recompilation() =
+
+    [<Theory>]
+    [<InlineData "">]
+    [<InlineData "()">]
+    [<InlineData "42">]
+    // [<InlineData "exit 0">]
+    // [<InlineData "printfn \"Hello world!\"">]
+    let ``Recompiles using restored TC info`` (code: string) =
+        let tempPath = $"{getTemporaryFileName()}.fsx"
+        
+        File.WriteAllText(tempPath, code) 
+
+        let cUnit =
+            FsxFromPath tempPath
+            |> withReuseTcResults
+            |> withOptions [ "--compressmetadata-" ]
+
+        let r1 =
+            cUnit
+            |> compileExisting
+            |> shouldSucceed
+            |> fun r -> ILChecker.generateIL r.Output.OutputPath.Value []
+
+        let r2 =
+            cUnit
+            |> compileExisting
+            |> shouldSucceed
+            |> fun r -> ILChecker.generateIL r.Output.OutputPath.Value []
+
+        Assert.Equal(r1, r2)
